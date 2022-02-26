df08842db3d94922316da4f64bf9fbbf9f159971
RetepMil <lkslyj2@naver.com>
2022-02-26 21:11:37 +0900

    fix: missing provider connection is now fixed

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -235,6 +235,7 @@ class Utils {
     }
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
